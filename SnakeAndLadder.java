80a719d78cc530c192f061584027ff8ece4fc8df
ashwini-8 <ashwinip309@gmail.com>
2020-12-05 13:55:05 +0530

    Added:action Ladder Snake Noplay

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,19 +4,47 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	// UC1 snake and ladder game played with single player at position 0
-
 	static final int STARTPOSITION = 0;
 	static final int PLAYER = 1;
+	static Random random = new Random();
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder Game");
-		System.out.println(rollingDice());
+		int diceNum = rollingDice();
+		System.out.println(diceNum);
+		checkPosition(diceNum);
 	}
 
 	// UC2 rolling a dice
 	static int rollingDice() {
-		Random rand = new Random();
-		int diceNum = rand.nextInt(6) + 1;
+		int diceNum = random.nextInt(6) + 1;
 		return diceNum;
+	}
+
+	// UC3 action no play,ladder,snake
+	static void checkPosition(int diceNum) {
+		int action = random.nextInt(3);
+		System.out.println(action);
+		int currentPosition = 0;
+
+		switch (action) {
+		case 0:
+			currentPosition = currentPosition;
+			System.out.println(currentPosition);
+			break;
+		case 1:
+			currentPosition = currentPosition + diceNum;
+			System.out.println(currentPosition);
+			break;
+		case 2:
+			if (currentPosition == 0) {
+				currentPosition = currentPosition;
+			} else {
+				currentPosition = currentPosition - diceNum;
+			}
+			System.out.println(currentPosition);
+			break;
+
+		}
 	}
 }
