9738cd659d102d39dcb0004195a373343c3111e6
ashwini-8 <ashwinip309@gmail.com>
2020-12-05 10:37:29 +0530

    Added: UC1 snake and ladder game played with single player at position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,12 @@
 package com.workshop.snakeandladder;
 
 public class SnakeAndLadder {
+	// UC1 snake and ladder game played with single player at position 0
+
+	static final int STARTPOSITION = 0;
+	static final int PLAYER = 1;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
